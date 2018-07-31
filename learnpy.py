bdb41f9afbbb02f5ae9e380acf44c339163af165
Thomas-LH <372084406@qq.com>
2018-07-31 20:52:02 +0800

    learning yield

--- learnpy.py
+++ learnpy.py
@@ -25,8 +25,36 @@ def dedupe(items, key=None):
             yield item
             seen.add(val)
 
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
 
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+    
+    def __iter__(self):
+        return iter(self._children)
+    
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
 
 if __name__ == '__main__':
-    a = [ {'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
-    print(list(dedupe(a, key=lambda d: (d['x'],d['y']))))
+#    a = [ {'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
+#    print(list(dedupe(a, key=lambda d: (d['x'],d['y']))))
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+
+    for ch in root.depth_first():
+        print(ch)
